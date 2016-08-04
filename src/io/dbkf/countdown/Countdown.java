f5bc6079de5d34e1a95d8b5fa86c8b5ae5802476
Ivan <ivan.dubc@gmail.com>
2016-08-05 00:27:47 +0300

    added default properties

--- src/io/dbkf/countdown/Countdown.java
+++ src/io/dbkf/countdown/Countdown.java
@@ -9,26 +9,72 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Countdown extends Application {
+	
+	private static Theme DEFAULT_THEME = Theme.DARK;
+	private static Long DEFAULT_SECONDS = 600L;
+	
+	private static Theme THEME;
+	private static Long SECONDS;
+	
+	enum Theme {
+		DARK("dark", "-fx-background: #000000;", "#FFFFFF"), 
+		LIGHT("light", "-fx-background: #FFFFFF;", "#000000");
+		private final String descr;
+		private final String fontColor;
+		private final String backgroundTheme;
+
+		Theme(String descr, String backgroundTheme, String fontColor) {
+			this.descr = descr;
+			this.backgroundTheme = backgroundTheme;
+			this.fontColor = fontColor;
+		}
+		public String getDescr() {
+			return descr;
+		}
+		
+		public String getFontColor() {
+			return fontColor;
+		}
+		
+		public String getBackgroundTheme() {
+			return backgroundTheme;
+		}
+	}
+	
+	/**
+	 * @param args
+	 * [0] - num of seconds
+	 * [1] - Theme (dark or light)
+	 */
 	public static void main(String[] args) {
+		SECONDS = args.length > 0 ? Long.valueOf(args[0]) : DEFAULT_SECONDS;
+		THEME = args.length > 1 ? toTheme(args[1]) : DEFAULT_THEME;
 		launch(args);
+	}
+
+	private static Theme toTheme(String themeNameFromArgs) {
+		for (Theme theme : Theme.values()) {
+			if (themeNameFromArgs.equals(theme.descr)) {
+				return theme;
+			}
+		}
+		throw new RuntimeException("Can't find theme with name " + themeNameFromArgs);
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parameters parameters = getParameters();
-		String initCountDownStr = parameters.getRaw().get(0);
-		long initCountdownTime = Long.valueOf(initCountDownStr);
 		stage.setTitle("Countdown");
 		stage.setFullScreen(true);
 		stage.centerOnScreen();
 		
 		StackPane root = new StackPane();
 		root.setPadding(new Insets(3));
-		root.setStyle("-fx-background: #000000;");
+		root.setStyle(THEME.getBackgroundTheme());
 
 		Scene scene = new Scene(root, 300, 250);
 		stage.setScene(scene);
@@ -36,8 +82,9 @@ public class Countdown extends Application {
 
 		long initTime = System.currentTimeMillis();
 
-		Label label = new Label(formatSeconds(getCountdown(initTime, initCountdownTime)));
+		Label label = new Label(formatSeconds(getCountdown(initTime, SECONDS)));
 		Font font = new Font("Ubuntu Condensed", 350);
+		label.setTextFill(Color.web(THEME.getFontColor()));
 		label.setFont(font);
 		root.getChildren().add(label);
 
@@ -48,7 +95,7 @@ public class Countdown extends Application {
 				Platform.runLater(new Runnable() {
 					@Override
 					public void run() {
-						long countdown = getCountdown(initTime, initCountdownTime);
+						long countdown = getCountdown(initTime, SECONDS);
 						if (countdown > 0) {
 							label.setText(formatSeconds(countdown));
 						} else {
